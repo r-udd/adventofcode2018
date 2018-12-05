98f75b1ea505c1acef37f1a160b05e0750675bda
Udd Robert <robert.udd@lm.se>
2018-12-05 17:34:04 +0100

    Go through list from end to beginning (Move fewer elements)

--- day5/both.py
+++ day5/both.py
@@ -2,15 +2,15 @@ def shouldBeRemoved(char1, char2):
     return char1.lower() == char2.lower() and char1 != char2
 
 def removePolymers (charList):
-    index = 0
+    index = len(charList) - 1
     while (True):
-        if index + 1 >= len(charList):
+        if index <= 0:
             break
-        if shouldBeRemoved(charList[index], charList[index+1]):
-            del charList[index: index + 2]
-            index = max(index - 1, 0)
+        if shouldBeRemoved(charList[index], charList[index-1]):
+            del charList[index-1: index + 1]
+            index = min(index - 1, len(charList) - 1)
         else:
-            index += 1
+            index -= 1
     return len(charList)
 
 with open('input') as f:
@@ -21,7 +21,6 @@ print('part1:', result1)
 
 minvalue = result1
 for char in 'abcdefghijklmnopqrstuvwxyz':
-    #currentline = line.replace(char,'').replace(char.upper(),'')
     charList = list(line.replace(char,'').replace(char.upper(),''))
     length = removePolymers(charList)
     if length < minvalue:
