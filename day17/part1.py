10a7ff4678e2a5ac8a263e8b3f2c6171bf8e4b3c
Udd Robert <robert.udd@lm.se>
2018-12-17 18:27:16 +0100

    Working day17 part1 test

--- day17/part1.py
+++ day17/part1.py
@@ -14,17 +14,15 @@ def findhole (area, coord, dir):
             return False, current
         current.x += dir
 
-def createmap(clay, movingwater, stillwater, minx, maxx):
+def createmap(clay, movingwater, minx, maxx, maxy):
     area = []
-    for y in range(clay[-1].y +1):
+    for y in range(maxy+1):
         arealine = []
         for x in range(minx, maxx+2):
             if Coord(x,y) in clay:
                 arealine.append('#')
-            elif Coord(x,y) in movingwater:
+            elif Coord(x,y) == movingwater:
                 arealine.append('|')
-            elif Coord(x,y) in stillwater:
-                arealine.append('~')
             elif x == 500 and y == 0:
                 arealine.append('+')
             else:
@@ -53,10 +51,20 @@ def findlowestmovingwater(area):
                 else:
                     right = '|'
                 
-                if below in ['#', '~'] and \
-                (left == '|' or right == '|') and (left == '.' or right == '.'):
-                    return Coord(x,y)
+                if below in ['#', '~']:
+                    if left == '.' or right == '.':
+                        return Coord(x,y)
+                    elif left == '#' and right == '#':
+                        return Coord(x,y)
+                        
 
+def countwater(area):
+    res = 0
+    for line in area:
+        for char in line:
+            if char in ['~', '|']:
+                res += 1
+    return res
 
 def printmap(area):
     for line in area:
@@ -65,8 +73,8 @@ def printmap(area):
         print()
     print()
 
-clay = []
-with open('day17/test') as f:
+clay = set()
+with open('day17/input') as f:
     for line in f:
         if line.startswith('x'):
             x = int(line[2:line.index(',')])
@@ -74,29 +82,32 @@ with open('day17/test') as f:
             ystart = int(line[line.index('y')+2:dotindex])
             ystop = int(line[dotindex + 2:-1])
             for y in range (ystart, ystop+1):
-                clay.append(Coord(x,y))
+                clay.add(Coord(x,y))
         elif line.startswith('y'):
             y = int(line[2:line.index(',')])
             dotindex = line.index('..')
             xstart = int(line[line.index('x')+2:dotindex])
             xstop = int(line[dotindex + 2:-1])
             for x in range (xstart, xstop+1):
-                clay.append(Coord(x,y))
+                clay.add(Coord(x,y))
 
 maxx = 0
 minx = 100000
+maxy = 0
 for c in clay:
     if c.x > maxx:
         maxx = c.x
     if c.x < minx:
         minx = c.x
+    if c.y > maxy:
+        maxy = c.y
 
-movingwater = [Coord(500,1)]
+#movingwater = [Coord(500,1)]
 
 #clay.sort()
 stillwater = []
-area = createmap(clay, movingwater, stillwater, minx, maxx)
-printmap(area)
+area = createmap(clay, Coord(500,1), minx, maxx, maxy)
+#printmap(area)
 while True:
     moving = findlowestmovingwater(area)
     if moving == None:
@@ -114,7 +125,9 @@ while True:
         if foundleft or foundright:
             for i in range(left.x, right.x+1):
                 area[moving.y][i] = '|'
-    printmap(area)
+    #printmap(area)
+
+print('Water amount', countwater(area))
 '''for water in movingwater:
     below = Coord(water.x, water.y+1)
     if below in movingwater:
