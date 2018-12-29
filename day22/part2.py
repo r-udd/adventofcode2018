2ebd98aa7f7cdc9c06075368f732ec793bad3513
Robert Udd <robert.udd@hotmail.com>
2018-12-29 18:25:44 +0100

    Day22 part 2 cleaning

--- day22/part2.py
+++ day22/part2.py
@@ -1,9 +1,7 @@
-import manhattan as m
-import coord as c
+#import manhattan as m
 import heapq
 
 tools = {'torch': '=', 'climb': '|', 'neither': '.'}
-#toolsallowed = {'=': {'climb', 'neither'}, '|': {'torch', 'neither'}, '.': {'climb', 'torch'}}
 
 dirs = ((-1, 0), (1, 0), (0, -1), (0, 1))
 
@@ -11,7 +9,6 @@ def ispassable(area, x, y, tool):
     return x >= 0 and y >= 0 and tools[tool] != area[y][x]
 
 def getadjecents(area, x, y, tool):
-    #possibles = (Coord(self.x, self.y-1), Coord(self.x-1, self.y), Coord(self.x+1, self.y), Coord(self.x, self.y+1))
     return ((x + d[0], y + d[1]) for d in dirs if ispassable(area, x + d[0], y + d[1], tool))
 
 def getregiontype(area, x, y):
@@ -22,15 +19,13 @@ def getnexttool(area, x, y, tool):
         if t != tool and getregiontype(area, x, y) != weaktype:
             return t
 
-def printmap (area, xrange, yrange, current, target, highlight):
+def printmap (area, xrange, yrange, current, target):
     for y in yrange:
         for x in xrange:
-            if x == current.x and y == current.y:
+            if x == current[0] and y == current[1]:
                 print('@', end='')
-            elif x == target.x and y == target.y:
+            elif x == target[0] and y == target[1]:
                 print('T', end='')
-            #elif c.Node(x,y,) in highlight:
-            #    print('X', end='')
             else:
                 print(area[y][x], end='')
         print()
@@ -38,40 +33,35 @@ def printmap (area, xrange, yrange, current, target, highlight):
 
 def astar(area, start, target):
     closedset = dict()
-    #open = getemptyadjecents(area, start)
     opennodes = []
-    #heapq.heappush(opennodes, start)
-    heapq.heappush(opennodes, (0, start.x, start.y, start.tool))
-    smallestmanhattan = 1000000
+    heapq.heappush(opennodes, (0, start[0], start[1], start[2]))
+    #smallestmanhattan = 1000000
     while opennodes:
-        #current = heapq.heappop(opennodes)
         minutes, x, y, tool = heapq.heappop(opennodes)
-        #print('f', current.f)
         key = (x, y, tool)
     
         if key in closedset and closedset[key] <= minutes:
             continue
         closedset[key] = minutes
-        if x == target.x and y == target.y and tool == target.tool:
+        if key == target:
             return minutes
 
         heapq.heappush(opennodes, (minutes+7, x, y, getnexttool(area, x, y, tool)))
-        #heapq.heappush(opennodes, c.Node(current.x, current.y, current.getnexttool(area), current.minutes+7))
         adjacents = getadjecents(area, x, y, tool)
         for adjacent in adjacents:
             
             adjacentminutes = minutes + 1
-            h = m.manhattan(adjacent[0], adjacent[1], target)
+            #h = m.manhattan(adjacent[0], adjacent[1], target)
             #if h < smallestmanhattan:
                 #print('manhattan dist to target', h)
                 #smallestmanhattan = h
 
             heapq.heappush(opennodes, (adjacentminutes, adjacent[0], adjacent[1], tool))
 
-def geologic (erosions, x, y, target):
+def geologic (erosions, x, y, targetx, targety):
     if x == 0 and y == 0:
         return 0
-    elif x == target.x and y == target.y:
+    elif x == targetx and y == targety:
         return 0
     elif y == 0:
         return x * 16807
@@ -80,26 +70,26 @@ def geologic (erosions, x, y, target):
     else:
         return erosions[x-1] * erosions[x]
 
-def erosion(x, y, erosions, depth, mod, target):
-    return (geologic(erosions, x, y, target) + depth) % mod
+def erosion(x, y, erosions, depth, mod, targetx, targety):
+    return (geologic(erosions, x, y, targetx, targety) + depth) % mod
 
-target = c.Node(14, 778, 'torch')#c.Coord(9,796)#c.Coord(14,778)#c.Coord(10, 10)#
-xsize = target.x + 1000
-ysize = target.y + 1000
+targetx = 14
+targety = 778
+target = (targetx, targety, 'torch')
+xsize = targetx + 1000
+ysize = targety + 1000
 erosions = [0] * xsize
 area = [[0] * xsize for x in range(ysize)]
-#erosions = [[0] * xsize for x in range(10)]
-#geologic = [[0] * xsize for x in range(10)]
-depth = 11541#510# #11541
+depth = 11541
 mod = 20183
 
 totalrisk = 0
 for y in range (ysize):
     for x in range (xsize):
-        erosions[x] = erosion(x, y, erosions, depth, mod, target)        
+        erosions[x] = erosion(x, y, erosions, depth, mod, targetx, targety)        
         risk = erosions[x] % 3
         totalrisk += risk
-        if x == target.x and y == target.y:
+        if x == targetx and y == targety:
             char = '.'
             #print('T', end='')
         elif x == 0 and y == 0:
@@ -118,28 +108,13 @@ for y in range (ysize):
     #print()
 
 
-start = c.Node(0,0, 'torch', 0)
+start = (0, 0, 'torch')
 
-printmap(area, range(target.x - 10, target.x + 10), range(target.y - 10, target.y + 10), start, target, [])
+printmap(area, range(targetx - 10, targetx + 10), range(targety - 10, targety + 10), start, target)
 #printmap(area, start, target, [])
-#while True:
-print('Search commencing!' + target.getregiontype(area))
+print('Search commencing!')
 
 endnode = astar(area, start, target)
 print('answer', endnode)
-#if endnode.tool != 'torch':
-#    totalcost += 7
-#highlight = []
-#while n.coord != start:
-#    highlight.append(n.coord)
-#    n = n.prev
-
 
 #printmap(area, range(target.coord.x - 10, target.coord.x + 10), range(target.coord.y - 10, target.coord.y + 10), start, target, highlight)
-#printmap(area, endnode.coord, target, highlight)
-#print('current tool ' + currenttool + ' cost: ' + str(totalcost))
-
-
-
-    #if nextcoord != target:
-        
